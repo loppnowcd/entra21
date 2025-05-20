992c4d25f289f4e1fda5c840be065b06b350970a
Christian Loppnow <loppnow.cd@gmail.com>
2025-05-20 19:33:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,38 @@
 class Calculadora {
-  somar(a, b) {
-    return a + b;
-  }
+            somar(a, b) {
+            return a + b;
+            }
 
-  subtrair(a, b) {
-    return a - b;
-  }
+            subtrair(a, b) {
+            return a - b;
+            }
 
-  multiplicar(a, b) {
-    return a * b;
-  }
+            multiplicar(a, b) {
+            return a * b;
+            }
 
-  dividir(a, b) {
-    return a / b;
-  }
-}
+            dividir(a, b) {
+            return a / b;
+            }
+            }
 
-const calculo = new Calculadora();
-  let resultadoCalculo;
+            function calculo() {
+            const valor1 = parseFloat(document.getElementById('primeiro-numero').value);
+            const valor2 = parseFloat(document.getElementById('segundo-numero').value);
+            const tipo = document.getElementById('tipoCalculo').value;
+            const calc = new Calculadora();
+            let resultado;
 
-    if (tipoCalculo == "somar") {
-    resultado = calculo.somar(valor1, valor2);
-    } 
-    else if (tipo == "subtrair") {
-    resultado = calculo.subtrair(valor1, valor2);
-    } 
-    else if (tipo == "multiplicar") {
-    resultado = calculo.multiplicar(valor1, valor2);
-    } 
-    else if (tipo == "dividir") {
-    resultado = calculo.dividir(valor1, valor2);
-    }
+            if (tipo == "somar") {
+            resultado = calc.somar(valor1, valor2);
+            } else if (tipo == "subtrair") {
+            resultado = calc.subtrair(valor1, valor2);
+            } else if (tipo == "multiplicar") {
+            resultado = calc.multiplicar(valor1, valor2);
+            } else if (tipo == "dividir") {
+            resultado = calc.dividir(valor1, valor2);
+            }
 
-//function chamarCalculo() {
-//
-//    let valor1 = document.getElementById('numValor1').value
-//    let valor2 = document.getElementById("numValor2").value
-//    let operacao = document.getElementById('selectOperacao').value
-//
-//    let resultadoOperacao = eval(parseInt(valor1) + operacao + parseInt(valor2))
-//
-//    let resultado = document.getElementById('numResultado')
-//
-//    resultado.value = resultadoOperacao
-//}
+            document.getElementById('resultado').value = resultado;
+            }
 
-
-
-function chamarCalculo() {
-
-    let valor1 = document.getElementById('numValor1').value
-    let valor2 = document.getElementById("numValor2").value
-    let operacao = document.getElementById('selectOperacao').value
-
-    let resultadoOperacao = eval(parseInt(valor1) + operacao + parseInt(valor2))
-
-    let resultado = document.getElementById('numResultado')
-
-    resultado.value = resultadoOperacao
-}
