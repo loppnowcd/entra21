590ec9149674b747ab1a9c9e10de913572bd9f27
Christian Loppnow <loppnow.cd@gmail.com>
2025-05-21 20:54:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
 class Calculadora {
-            somar(a, b) {
-            return a + b;
-            }
+  somar(a, b) {
+    return a + b;
+  }
 
-            subtrair(a, b) {
-            return a - b;
-            }
+  subtrair(a, b) {
+    return a - b;
+  }
 
-            multiplicar(a, b) {
-            return a * b;
-            }
+  multiplicar(a, b) {
+    return a * b;
+  }
 
-            dividir(a, b) {
-            return a / b;
-            }
-            }
+  dividir(a, b) {
+    return a / b;
+  }
+}
 
-            function calculo() {
-            const valor1 = parseFloat(document.getElementById('primeiro-numero').value);
-            const valor2 = parseFloat(document.getElementById('segundo-numero').value);
-            const tipo = document.getElementById('tipoCalculo').value;
-            const calc = new Calculadora();
-            let resultado;
+const calculo = new Calculadora();
+  let resultadoCalculo;
 
-            if (tipo == "somar") {
-            resultado = calc.somar(valor1, valor2);
-            } else if (tipo == "subtrair") {
-            resultado = calc.subtrair(valor1, valor2);
-            } else if (tipo == "multiplicar") {
-            resultado = calc.multiplicar(valor1, valor2);
-            } else if (tipo == "dividir") {
-            resultado = calc.dividir(valor1, valor2);
-            }
+    if (tipoCalculo == "somar") {
+    resultado = calculo.somar(valor1, valor2);
+    } 
+    else if (tipo == "subtrair") {
+    resultado = calculo.subtrair(valor1, valor2);
+    } 
+    else if (tipo == "multiplicar") {
+    resultado = calculo.multiplicar(valor1, valor2);
+    } 
+    else if (tipo == "dividir") {
+    resultado = calculo.dividir(valor1, valor2);
+    }
 
-            document.getElementById('resultado').value = resultado;
-            }
+//function chamarCalculo() {
+//
+//    let valor1 = document.getElementById('numValor1').value
+//    let valor2 = document.getElementById("numValor2").value
+//    let operacao = document.getElementById('selectOperacao').value
+//
+//    let resultadoOperacao = eval(parseInt(valor1) + operacao + parseInt(valor2))
+//
+//    let resultado = document.getElementById('numResultado')
+//
+//    resultado.value = resultadoOperacao
+//}
 
 
-            //Supondo que fosse fazer uma função sem classe
-            //function chamarCalculo() {
-            //let valor1 = document.getElementById('numValor1').value
-            //let valor2 = document.getElementById("numValor2").value
-            //let operacao = document.getElementById('selectOperacao').value
-
-            //let resultadoOperacao = eval(parseInt(valor1) + operacao + parseInt(valor2))
-
-            //let resultado = document.getElementById('numResultado')
-
-            //resultado.value = resultadoOperacao
+            //function inserir(valor) {
+            //document.getElementById("resultado").value += valor;
             //}
-
+            //
+            //function calcular() {
+            //let expressao = document.getElementById("resultado").value;
+            //
+            //document.getElementById("resultado").value = eval(expressao);
+            //}
